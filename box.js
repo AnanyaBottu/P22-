c9d44066518754b40814559eaa3e79fef5701899
AnanyaBottu <58173885+AnanyaBottu@users.noreply.github.com>
2021-01-18 07:55:32 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,18 @@
+class Box {
+    constructor(x, y, width, height) {
+      this.body = Bodies.rectangle(x, y, width, height);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var Position =this.body.position;
+      push();
+      translate(Position.x,Position.y);
+      fill(255, 0, 165);
+      rectMode(CENTER)
+      rect(0,0, this.width, this.height);
+      pop();
+    }
+  };
